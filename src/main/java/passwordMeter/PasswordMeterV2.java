b865d36328118f90c7082f98e18282b1e6549290
Pedro Neto <pedro.santosnt@hotmail.com>
2021-05-09 03:15:59 -0300

    Rename PasswordMeterV3 to PasswordMeterV2

--- src/main/java/passwordMeter/PasswordMeterV2.java
+++ src/main/java/passwordMeter/PasswordMeterV2.java
@@ -1,338 +1,89 @@
 package passwordMeter;
 
 public class PasswordMeterV2 {
+    String password;
+    String sComplexity;
 
-    public int score;
-    public String password;
-    public String sComplexity = "Too Short";
+    int score;
+    int bonusLength, countLength;
+    int bonusAlphaUpperCase, countAlphaUpperCase;
+    int bonusAlphaLowerCase, countAlphaLowerCase;
+    int bonusNumber, countNumber;
+    int bonusSymbol, countSymbol;
+    int bonusMidChar, countMidChar;
+    int bonusRequirements, countRequirements;
 
-    // Results
-    public int bonusLength, countLength;
-    public int bonusAlphaUpperCase, countAlphaUpperCase;
-    public int bonusAlphaLowerCase, countAlphaLowerCase;
-    public int bonusNumber, countNumber;
-    public int bonusSymbol, countSymbol;
-    public int bonusMidChar, countMidChar;
-    public int bonusRequirements, countRequirements;
-    public int bonusAlphasOnly, countAlphasOnly;
-    public int bonusNumbersOnly, countNumbersOnly;
-    public int bonusRepChar, countRepChar;
-    public int bonusConsecutiveAlphaUC, countConsecutiveAlphaUC;
-    public int bonusConsecutiveAlphaLC, countConsecutiveAlphaLC;
-    public int bonusConsecutiveNumber, countConsecutiveNumber;
-    public int bonusSeqAlpha, countSeqAlpha;
-    public int bonusSeqNumber, countSeqNumber;
-    public int bonusSeqSymbol, countSeqSymbol;
+    int bonusAlphasOnly, countAlphasOnly;
+    int countNumbersOnly, bonusNumbersOnly;
+    int countRepChar, bonusRepChar;
+    int countConsecutiveAlphaUC, bonusConsecutiveAlphaUC;
+    int countConsecutiveAlphaLC, bonusConsecutiveAlphaLC;
+    int countConsecutiveNumber, bonusConsecutiveNumber;
+    int countSeqAlpha, bonusSeqAlpha;
+    int countSeqNumber, bonusSeqNumber;
+    int countSeqSymbol, bonusSeqSymbol;
 
-    public String[] levelsOfMandatoryItems = new String[5];
-    public String[] levelsOfBonusItems = new String[2];
-    public String[] levelsOfSuggestedItems = new String[9];
+    public PasswordMeterV2(String password) {
+        this.password = password;
 
-    Integer nTmpAlphaUC = null, nTmpAlphaLC = null, nTmpNumber = null;
-    double incrementDeductionOfRepeatedChars = 0;
+        // Additions
+        countLength = new NumberOfCharacters(password).getCount();
+        bonusLength = new NumberOfCharacters(password).getBonus();
 
-    public void numberOfCharacters() {
-        int multiplierLength = 4;
-        countLength = password.length();
-        score = password.length() * multiplierLength;
-    }
+        countAlphaUpperCase = new UppercaseLetters(password).getCount();
+        bonusAlphaUpperCase = new UppercaseLetters(password).getBonus();
 
-    public void countOfConsecutiveLettersUpperCased(String[] arrPwd, int i) {
-        if (arrPwd[i].matches("[A-Z]")) {
-            if (nTmpAlphaUC != null) {
-                if (nTmpAlphaUC + 1 == i) {
-                    countConsecutiveAlphaUC++;
-                }
-            }
-            nTmpAlphaUC = i;
-            countAlphaUpperCase++;
-        }
-    }
+        countAlphaLowerCase = new LowercaseLetters(password).getCount();
+        bonusAlphaLowerCase = new LowercaseLetters(password).getBonus();
 
-    public void countOfConsecutiveLettersLowerCased(String[] arrPwd, int i) {
-        if (arrPwd[i].matches("[a-z]")) {
-            if (nTmpAlphaLC != null) {
-                if (nTmpAlphaLC + 1 == i) {
-                    countConsecutiveAlphaLC++;
-                }
-            }
-            nTmpAlphaLC = i;
-            countAlphaLowerCase++;
-        }
-    }
+        countNumber = new Numbers(password).getCount();
+        bonusNumber = new Numbers(password).getBonus();
 
-    public void countOfConsecutiveNumbers(String[] arrPwd, int i) {
-        if (arrPwd[i].matches("[0-9]")) {
-            if (i > 0 && i < arrPwd.length - 1) {
-                countMidChar++;
-            }
-            if (nTmpNumber != null) {
-                if (nTmpNumber + 1 == i) {
-                    countConsecutiveNumber++;
-                }
-            }
-            nTmpNumber = i;
-            countNumber++;
-        }
-    }
+        countSymbol = new Symbols(password).getCount();
+        bonusSymbol = new Symbols(password).getBonus();
 
-    public void countOfSymbols(String[] arrPwd, int i) {
-        if (arrPwd[i].matches("[^a-zA-Z0-9_]")) {
-            if (i > 0 && i < arrPwd.length - 1) {
-                countMidChar++;
-            }
-            countSymbol++;
-        }
-    }
+        countMidChar = new MiddleNumbersOrSymbols(password).getCount();
+        bonusMidChar = new MiddleNumbersOrSymbols(password).getBonus();
 
-    public void countOfRepeatedCharacters(String[] arrPwd, int i) {
-        var bCharExists = false;
-        for (int j = 0; j < arrPwd.length; j++) {
-            if (arrPwd[i].equals(arrPwd[j]) && i != j) { /* repeat character exists */
-                bCharExists = true;
+        countRequirements = new Requirements(password).getCount();
+        bonusRequirements = new Requirements(password).getBonus();
 
-                incrementDeductionOfRepeatedChars += Math.abs(arrPwd.length / (j - i));
-            }
-        }
-        if (bCharExists) {
-            countRepChar++;
-            int countUniqueCharacters = arrPwd.length - countRepChar;
-            incrementDeductionOfRepeatedChars = countUniqueCharacters != 0 ?
-                    Math.ceil(incrementDeductionOfRepeatedChars / countUniqueCharacters) :
-                    Math.ceil(incrementDeductionOfRepeatedChars);
-        }
-    }
+        // Deductions
+        countAlphasOnly = new LettersOnly(password).getCount();
+        bonusAlphasOnly = new LettersOnly(password).getBonus();
 
-    public void checkSequentialLettersPattern() {
-        /* Check for sequential alpha string patterns (forward and reverse) */
-        for (int i = 0; i < 23; i++) {
-            String ALPHAS = "abcdefghijklmnopqrstuvwxyz";
-            String sFwd = ALPHAS.substring(i, i + 3);
-            String sRev = new StringBuilder(sFwd).reverse().toString();
-            if (password.toLowerCase().contains(sFwd) || password.toLowerCase().contains(sRev)) {
-                countSeqAlpha++;
-            }
-        }
-    }
+        countNumbersOnly = new NumbersOnly(password).getCount();
+        bonusNumbersOnly = new NumbersOnly(password).getBonus();
 
-    public void checkSequentialNumericPattern() {
-        /* Check for sequential numeric string patterns (forward and reverse) */
-        for (int i = 0; i < 8; i++) {
-            String DIGITS = "01234567890";
-            String sFwd = DIGITS.substring(i, i + 3);
-            String sRev = new StringBuilder(sFwd).reverse().toString();
-            if (password.toLowerCase().contains(sFwd) || password.toLowerCase().contains(sRev)) {
-                countSeqNumber++;
-            }
-        }
-    }
+        countRepChar = new RepeatCharacters(password).getCount();
+        bonusRepChar = new RepeatCharacters(password).getBonus();
 
-    public void checkSequentialSymbolPattern() {
-        /* Check for sequential symbol string patterns (forward and reverse) */
-        for (int i = 0; i < 8; i++) {
-            String SYMBOLS = ")!@#$%^&*()";
-            String sFwd = SYMBOLS.substring(i, i + 3);
-            String sRev = new StringBuilder(sFwd).reverse().toString();
-            if (password.toLowerCase().contains(sFwd) || password.toLowerCase().contains(sRev)) {
-                countSeqSymbol++;
-            }
-        }
-    }
+        countConsecutiveAlphaUC = new ConsecutiveUppercaseLetters(password).getCount();
+        bonusConsecutiveAlphaUC = new ConsecutiveUppercaseLetters(password).getBonus();
 
-    public void calculateBonusLetterUpperCase() {
-        if (countAlphaUpperCase > 0 && countAlphaUpperCase < countLength) {
-            score = score + (countLength - countAlphaUpperCase) * 2;
-            bonusAlphaUpperCase = (countLength - countAlphaUpperCase) * 2;
-        }
-    }
+        countConsecutiveAlphaLC = new ConsecutiveLowercaseLetters(password).getCount();
+        bonusConsecutiveAlphaLC = new ConsecutiveLowercaseLetters(password).getBonus();
 
-    public void calculateBonusLetterLowerCase() {
-        if (countAlphaLowerCase > 0 && countAlphaLowerCase < countLength) {
-            score = score + (countLength - countAlphaLowerCase) * 2;
-            bonusAlphaLowerCase = (countLength - countAlphaLowerCase) * 2;
-        }
-    }
+        countConsecutiveNumber = new ConsecutiveNumbers(password).getCount();
+        bonusConsecutiveNumber = new ConsecutiveNumbers(password).getBonus();
 
-    public void calculateBonusNumber() {
-        if (countNumber > 0 && countNumber < countLength) {
-            int multiplierNumber = 4;
-            score = score + countNumber * multiplierNumber;
-            bonusNumber = countNumber * multiplierNumber;
-        }
-    }
+        countSeqAlpha = new SequentialLetters(password).getCount();
+        bonusSeqAlpha = new SequentialLetters(password).getBonus();
 
-    public void calculateBonusSymbol() {
-        if (countSymbol > 0) {
-            int multiplierSymbol = 6;
-            score = score + countSymbol * multiplierSymbol;
-            bonusSymbol = countSymbol * multiplierSymbol;
-        }
-    }
+        countSeqNumber = new SequentialNumbers(password).getCount();
+        bonusSeqNumber = new SequentialNumbers(password).getBonus();
 
-    public void calculateBonusMidCharacter() {
-        if (countMidChar > 0) {
-            int multiplierMidChar = 2;
-            score = score + countMidChar * multiplierMidChar;
-            bonusMidChar = countMidChar * multiplierMidChar;
-        }
-    }
+        countSeqSymbol = new SequentialSymbols(password).getCount();
+        bonusSeqSymbol = new SequentialSymbols(password).getBonus();
 
-    public void calculateDeductions() {
-        if ((countAlphaLowerCase > 0 || countAlphaUpperCase > 0) && countSymbol == 0 && countNumber == 0) {  // Only Letters
-            score = score - countLength;
-            countAlphasOnly = countLength;
-            bonusAlphasOnly = countLength;
-        }
-        if (countAlphaLowerCase == 0 && countAlphaUpperCase == 0 && countSymbol == 0 && countNumber > 0) {  // Only Numbers
-            score = score - countLength;
-            countNumbersOnly = countLength;
-            bonusNumbersOnly = countLength;
-        }
-        if (countRepChar > 0) {  // Same character exists more than once
-            score = score - (int) incrementDeductionOfRepeatedChars;
-            bonusRepChar = (int) incrementDeductionOfRepeatedChars;
-        }
-        if (countConsecutiveAlphaUC > 0) {  // Consecutive Uppercase Letters exist
-            int multiplierConsecutiveAlphaUC = 2;
-            score = score - countConsecutiveAlphaUC * multiplierConsecutiveAlphaUC;
-            bonusConsecutiveAlphaUC = countConsecutiveAlphaUC * multiplierConsecutiveAlphaUC;
-        }
-        if (countConsecutiveAlphaLC > 0) {  // Consecutive Lowercase Letters exist
-            int multiplierConsecutiveAlphaLC = 2;
-            score = score - countConsecutiveAlphaLC * multiplierConsecutiveAlphaLC;
-            bonusConsecutiveAlphaLC = countConsecutiveAlphaLC * multiplierConsecutiveAlphaLC;
-        }
-        if (countConsecutiveNumber > 0) {  // Consecutive Numbers exist
-            int multiplierConsecutiveNumber = 2;
-            score = score - countConsecutiveNumber * multiplierConsecutiveNumber;
-            bonusConsecutiveNumber = countConsecutiveNumber * multiplierConsecutiveNumber;
-        }
-        if (countSeqAlpha > 0) {  // Sequential alpha strings exist (3 characters or more)
-            int multiplierSeqAlpha = 3;
-            score = score - countSeqAlpha * multiplierSeqAlpha;
-            bonusSeqAlpha = countSeqAlpha * multiplierSeqAlpha;
-        }
-        if (countSeqNumber > 0) {  // Sequential numeric strings exist (3 characters or more)
-            int multiplierSeqNumber = 3;
-            score = score - countSeqNumber * multiplierSeqNumber;
-            bonusSeqNumber = countSeqNumber * multiplierSeqNumber;
-        }
-        if (countSeqSymbol > 0) {  // Sequential symbol strings exist (3 characters or more)
-            int multiplierSeqSymbol = 3;
-            score = score - countSeqSymbol * multiplierSeqSymbol;
-            bonusSeqSymbol = countSeqSymbol * multiplierSeqSymbol;
-        }
-    }
-
-    public void calculatePasswordComplexity() {
-        if (score > 100) {
-            score = 100;
-        } else if (score < 0) {
-            score = 0;
-        }
-
-        if (score < 20) {
-            sComplexity = "Very Weak";
-        } else if (score < 40) {
-            sComplexity = "Weak";
-        } else if (score < 60) {
-            sComplexity = "Good";
-        } else if (score < 80) {
-            sComplexity = "Strong";
-        } else {
-            sComplexity = "Very Strong";
-        }
-    }
-
-    public void checkPassword(String candidate) {
-        password = candidate;
-
-        numberOfCharacters();
-
-        String[] arrPwd = candidate.replaceAll("\\s+", "").split("\\s*");
-        for (int i = 0; i < arrPwd.length; i++) {
-            countOfConsecutiveLettersUpperCased(arrPwd, i);
-            countOfConsecutiveLettersLowerCased(arrPwd, i);
-            countOfConsecutiveNumbers(arrPwd, i);
-            countOfSymbols(arrPwd, i);
-            countOfRepeatedCharacters(arrPwd, i);
-        }
-
-        checkSequentialLettersPattern();
-        checkSequentialNumericPattern();
-        checkSequentialSymbolPattern();
-
-        /* Modify overall score value based on usage vs requirements */
-        /* General point assignment */
-        bonusLength = score;
-
-        // Calculate bonus
-        calculateBonusLetterUpperCase();
-        calculateBonusLetterLowerCase();
-        calculateBonusNumber();
-        calculateBonusSymbol();
-        calculateBonusMidCharacter();
-
-        /* Point deductions for poor practices */
-        calculateDeductions();
-
-        /* Determine if mandatory requirements have been met and set image indicators accordingly */
-        int[] arrChars = {countLength, countAlphaUpperCase, countAlphaLowerCase, countNumber, countSymbol};
-        String[] arrCharsIds = {"countLength", "countAlphaUC", "countAlphaLC", "countNumber", "countSymbol"};
-        var arrCharsLen = arrChars.length;
-        int MINIMUM_LENGTH = 8;
-        for (int i = 0; i < arrCharsLen; i++) {
-
-            int minVal = arrCharsIds[i].equals("countLength") ? MINIMUM_LENGTH - 1 : 0;
-            if (arrChars[i] == minVal + 1) {
-                countRequirements++;
-                levelsOfMandatoryItems[i] = "sufficient";
-            } else if (arrChars[i] > minVal + 1) {
-                countRequirements++;
-                levelsOfMandatoryItems[i] = "exceptional";
-            } else {
-                levelsOfMandatoryItems[i] = "failure";
-            }
-        }
-
-        int nMinReqChars = candidate.length() >= MINIMUM_LENGTH ? 3 : 4;
-        if (countRequirements > nMinReqChars) {  // One or more required characters exist
-            score = score + countRequirements * 2;
-            bonusRequirements = countRequirements * 2;
-        }
-
-        /* Determine if additional bonuses need to be applied and set image indicators accordingly */
-        arrChars = new int[]{countMidChar, countRequirements};
-        arrCharsIds = new String[]{"countMidChar", "countRequirements"};
-        arrCharsLen = arrChars.length;
-        for (var c = 0; c < arrCharsLen; c++) {
-            int minVal = arrCharsIds[c].equals("countRequirements") ? nMinReqChars : 0;
-            if (arrChars[c] == minVal + 1) {
-                levelsOfBonusItems[c] = "sufficient";
-            } else if (arrChars[c] > minVal + 1) {
-                levelsOfBonusItems[c] = "exceptional";
-            } else {
-                levelsOfBonusItems[c] = "failure";
-            }
-        }
-
-        /* Determine if suggested requirements have been met and set image indicators accordingly */
-        arrChars = new int[]{countAlphasOnly, countNumbersOnly, countRepChar, countConsecutiveAlphaUC,
-                countConsecutiveAlphaLC, countConsecutiveNumber, countSeqAlpha, countSeqNumber, countSeqSymbol};
-        arrCharsLen = arrChars.length;
-        for (var c = 0; c < arrCharsLen; c++) {
-            levelsOfSuggestedItems[c] = arrChars[c] > 0 ? "warning" : "sufficient";
-        }
-
-        /* Determine complexity based on overall score */
-        calculatePasswordComplexity();
-
+        score = new Score(password).calculateScore();
+        sComplexity = new Complexity(score).calculatePasswordComplexity();
     }
 
     @Override
     public String toString() {
         return "Password: " + password
-                + "\nScore: " + score
+                + "\nScore: " + score + ""
                 + "\nComplexity: " + sComplexity
 
                 + "\nAddictions"
@@ -356,3 +107,4 @@ public class PasswordMeterV2 {
                 + "\n[C: " + countSeqSymbol + " | B: " + bonusSeqSymbol + "] Sequential Symbols";
     }
 }
+
--- src/test/java/passwordMeter/PasswordMeterV2Test.java
+++ src/test/java/passwordMeter/PasswordMeterV2Test.java
@@ -31,15 +31,13 @@ class PasswordMeterV2Test {
                 "[C: 4 | B: 12] Sequential Numbers\n" +
                 "[C: 0 | B: 0] Sequential Symbols";
 
-        var meter = new PasswordMeterV2();
-        meter.checkPassword("123456");
+        var meter = new PasswordMeterV2("123456");
 //        System.out.println(meter);
         assertEquals(expected, meter.toString());
     }
 
     @Test
-    void checkBiggerPassword() {
-
+    void main() {
         var expected = "Password: asdfhjkiK4123*@^DFDS\n" +
                 "Score: 100\n" +
                 "Complexity: Very Strong\n" +
@@ -62,9 +60,40 @@ class PasswordMeterV2Test {
                 "[C: 1 | B: 3] Sequential Numbers\n" +
                 "[C: 0 | B: 0] Sequential Symbols";
 
-        var meter = new PasswordMeterV2();
-        meter.checkPassword("asdfhjkiK4123*@^DFDS");
-//        System.out.println(meter);
-        assertEquals(expected, meter.toString());
+
+        var result = new PasswordMeterV2("asdfhjkiK4123*@^DFDS");
+//        System.out.println(result);
+        assertEquals(expected, result.toString());
     }
+
+    @Test
+    void main2() {
+        var expected = "Password: asd123@EWQ\n" +
+                "Score: 89\n" +
+                "Complexity: Very Strong\n" +
+                "Addictions\n" +
+                "[C: 10 | B: 40] Number of Characters\n" +
+                "[C: 3 | B: 14] Uppercase Letters\n" +
+                "[C: 3 | B: 14] Lowercase Letters\n" +
+                "[C: 3 | B: 12] Numbers\n" +
+                "[C: 1 | B: 6] Symbols\n" +
+                "[C: 4 | B: 8] Middle Numbers or Symbols\n" +
+                "[C: 5 | B: 10] Requirements\n" +
+                "Deductions\n" +
+                "[C: 0 | B: 0] Letters Only\n" +
+                "[C: 0 | B: 0] Numbers Only\n" +
+                "[C: 0 | B: 0] Repeat Characters (Case Insensitive)\n" +
+                "[C: 2 | B: 4] Consecutive Uppercase Letters\n" +
+                "[C: 2 | B: 4] Consecutive Lowercase Letters\n" +
+                "[C: 2 | B: 4] Consecutive Numbers\n" +
+                "[C: 0 | B: 0] Sequential Letters\n" +
+                "[C: 1 | B: 3] Sequential Numbers\n" +
+                "[C: 0 | B: 0] Sequential Symbols";
+
+        var result = new PasswordMeterV2("asd123@EWQ");
+//        System.out.println(result);
+        assertEquals(expected, result.toString());
+    }
+
+
 }
